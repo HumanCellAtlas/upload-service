4c5fb8618cad52d4405a8699ac979dc1cba858f4
Sam Pierson <sam@sampierson.com>
2017-09-13 21:02:27 -0700

    Un-hardwire DEPLOYMENT_STAGE in travis.yml

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
+if [[ $# != 1 ]]; then
+    echo "Usage: $(basename $0) stage"
+    exit 1
+fi
+
+export DEPLOYMENT_STAGE=$1
+source config/environment
+openssl aes-256-cbc -k $enc_password -in config/deployment_secrets.${DEPLOYMENT_STAGE}.enc -out config/deployment_secrets.${DEPLOYMENT_STAGE} -d
 source config/deployment_secrets.${DEPLOYMENT_STAGE}
 make deploy
