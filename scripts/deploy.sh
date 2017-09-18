8da7700ebf4a52d0ce4022c8cc5cf85e551e090c
Sam Pierson <sam@sampierson.com>
2017-09-18 10:49:39 -0700

    Tag deploys

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,8 +5,22 @@ if [[ $# != 1 ]]; then
     exit 1
 fi
 
+function tag_deploy(){
+    curl -X POST \
+         --header "Authorization: token ${GITHUB_ACCESS_TOKEN}" \
+         --header "Content-Type: application/json" \
+         --data @- \
+         https://api.github.com/repos/HumanCellAtlas/staging-service/git/refs <<-EOF
+{
+  "ref": "`date -u +"refs/tags/${DEPLOYMENT_STAGE}-%Y%m%dT%H%M%SZ"`",
+  "sha": "`git rev-parse HEAD`"
+}
+EOF
+}
+
 export DEPLOYMENT_STAGE=$1
 source config/environment
 openssl aes-256-cbc -k $enc_password -in config/deployment_secrets.${DEPLOYMENT_STAGE}.enc -out config/deployment_secrets.${DEPLOYMENT_STAGE} -d
 source config/deployment_secrets.${DEPLOYMENT_STAGE}
 make deploy
+tag_deploy
