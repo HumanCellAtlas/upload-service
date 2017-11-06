6e013cfdad22c8a8b03e12780d4ef63fe84f60a2
Sam Pierson <sam@sampierson.com>
2017-11-06 10:02:23 -0800

    Refactor scripts/deploy.sh for readability

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,16 @@ if [[ $# != 1 ]]; then
     exit 1
 fi
 
+export DEPLOYMENT_STAGE=$1
+
+function load_secrets(){
+    secrets_file="config/deployment_secrets.${DEPLOYMENT_STAGE}"
+    if [ ! -f ${secrets_file} ] ; then
+        openssl aes-256-cbc -k ${enc_password} -in ${secrets_file}.enc -out ${secrets_file} -d
+    fi
+    source ${secrets_file}
+}
+
 function tag_deploy(){
     curl -X POST \
          --header "Authorization: token ${GITHUB_ACCESS_TOKEN}" \
@@ -18,10 +28,8 @@ function tag_deploy(){
 EOF
 }
 
-export DEPLOYMENT_STAGE=$1
 source config/environment
-openssl aes-256-cbc -k $enc_password -in config/deployment_secrets.${DEPLOYMENT_STAGE}.enc -out config/deployment_secrets.${DEPLOYMENT_STAGE} -d
-source config/deployment_secrets.${DEPLOYMENT_STAGE}
+load_secrets
 make deploy
 if [ ${DEPLOYMENT_STAGE} != dev ] ; then
     tag_deploy
