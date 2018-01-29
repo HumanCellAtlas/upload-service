5bbc6192c8f33f59ca82e34f9962dc43e0e15577
Sam Pierson <sam@sampierson.com>
2018-01-29 11:51:43 -0800

    uploadctl setup upload-api now controls the rest of the API gateway

--- uploadctl/setup/upload_api.py
+++ uploadctl/setup/upload_api.py
@@ -3,6 +3,8 @@ import os
 from .component import CompositeComponent, ExternalControl
 from .aws.llambda import Lambda
 from .aws.iam import IAMRole, RoleInlinePolicy
+from .aws.api_gateway import DomainName, BasePathMapping, RestApi
+from .aws.acm import SslCertificate
 
 
 class ApiLambda(Lambda):
@@ -29,12 +31,42 @@ class ApiLambdaRolePolicy(RoleInlinePolicy):
         raise ExternalControl("Use \"make deploy\" to set this up")
 
 
+class ApiSSLCert(SslCertificate):
+    def __init__(self):
+        wildcard_cert_domain = ".".join(['*'] + os.environ['API_HOST'].split('.')[1:])
+        super().__init__(domain=wildcard_cert_domain)
+
+
+class ApiDomain(DomainName):
+    def __init__(self):
+        super().__init__(domain=os.environ['API_HOST'])
+
+
+class UploadRestApi(RestApi):
+    def __init__(self, quiet=False):
+        super().__init__(name='upload.api_server', stage=os.environ['DEPLOYMENT_STAGE'], quiet=quiet)
+
+
+class ApiBasePathMapping(BasePathMapping):
+    def __init__(self):
+        rest_api = UploadRestApi(quiet=True)
+        super().__init__(
+            domain_name=os.environ['API_HOST'],
+            base_path='',
+            rest_api_id=rest_api.id
+        )
+
+
 class UploadApi(CompositeComponent):
 
     SUBCOMPONENTS = {
         'api-lambda': ApiLambda,
         'apl-lambda-role': ApiLambdaRole,
         'apl-lambda-role-policy': ApiLambdaRolePolicy,
+        'ssl-cert': ApiSSLCert,
+        'rest-api': UploadRestApi,
+        'custom-domain': ApiDomain,
+        'base-path-mapping': ApiBasePathMapping,
     }
 
     def __str__(self):
