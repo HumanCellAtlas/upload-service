6a80615d594c3426f8455d8d3631b48c426d47e6
Parth Shah <parths@chanzuckerberg.com>
2018-04-30 15:29:22 -0700

    changes/additions to upload api and ingest notifier that is now called from the upload api

--- upload/common/ingest_notifier.py
+++ upload/common/ingest_notifier.py
@@ -17,8 +17,18 @@ class IngestNotifier:
     FILE_UPLOAD_EXCHANGE = 'ingest.file.staged.exchange'
     FILE_UPLOADED_QUEUE = 'ingest.file.create.staged'
 
-    def __init__(self):
-        self.notification_id = str(uuid.uuid4())
+    FILE_VALIDATED_EXCHANGE = "ingest.validation.exchange"
+    FILE_VALIDATED_QUEUE = "ingest.file.validation.queue"
+
+    def __init__(self, notification_type):
+        if notification_type == "file_uploaded":
+            self.exchange = self.FILE_UPLOAD_EXCHANGE
+            self.queue = self.FILE_UPLOADED_QUEUE
+        elif notification_type == "file_validated":
+            self.exchange = self.FILE_VALIDATED_EXCHANGE
+            self.queue = self.FILE_VALIDATED_QUEUE
+        else:
+            raise Exception("Unknown notification type for ingest")
         self.ingest_amqp_server = os.environ['INGEST_AMQP_SERVER']
         logger.debug("starting")
         self.connect()
@@ -29,43 +39,37 @@ class IngestNotifier:
         logger.debug(f"got connection {self.connection}")
         self.channel = self.connection.channel()
         logger.debug(f"got channel {self.channel}")
-        retval = self.channel.queue_declare(queue=self.FILE_UPLOADED_QUEUE)
-        logger.debug(f"declaring queue {self.FILE_UPLOADED_QUEUE} returned {retval}")
+        retval = self.channel.queue_declare(queue=self.queue)
+        logger.debug(f"declaring queue {self.queue} returned {retval}")
 
-    def file_was_uploaded(self, file_info):
-        upload_area_id = file_info["upload_area_id"]
-        file_name = file_info["name"]
-        self.file_key = upload_area_id + "/" + file_name
-        format_logger_with_id(logger, "file_key", self.file_key)
-        self.payload = file_info
-        self.status = "DELIVERING"
-        self._create_record()
+    def format_and_send_notification(self, file_info):
+        notification_props = self._format_notification_props(file_info)
+        notification_props["status"] = "DELIVERING"
+        format_logger_with_id(logger, "file_key", notification_props["file_id"])
+        create_pg_record("notification", notification_props)
         body = json.dumps(file_info)
-        success = self.channel.basic_publish(exchange=self.FILE_UPLOAD_EXCHANGE,
-                                             routing_key=self.FILE_UPLOADED_QUEUE,
+        success = self._publish_notification(body)
+        notification_props["status"] = "DELIVERED"
+        update_pg_record("notification", notification_props)
+        return success
+
+    def _publish_notification(self, body):
+        success = self.channel.basic_publish(exchange=self.exchange,
+                                             routing_key=self.queue,
                                              body=body)
         logger.debug(f"publish of {body} returned {success}")
         if not success:
             raise UploadException(status=requests.codes.server_error, title="Unexpected Error",
                                   detail=f"basic_publish to {self.ingest_amqp_server} returned {success}")
         self.connection.close()
-        self.status = "DELIVERED"
-        self._update_record()
         return success
 
-    def _format_prop_vals_dict(self):
-        vals_dict = {
-            "id": self.notification_id,
-            "payload": self.payload,
-            "file_id": self.file_key,
-            "status": self.status
+    def _format_notification_props(self, file_info):
+        upload_area_id = file_info["upload_area_id"]
+        file_name = file_info["name"]
+        notification_props = {
+            "id": str(uuid.uuid4()),
+            "file_id": f"{upload_area_id}/{file_name}",
+            "payload": file_info
         }
-        return vals_dict
-
-    def _create_record(self):
-        prop_vals_dict = self._format_prop_vals_dict()
-        create_pg_record("notification", prop_vals_dict)
-
-    def _update_record(self):
-        prop_vals_dict = self._format_prop_vals_dict()
-        update_pg_record("notification", prop_vals_dict)
+        return notification_props
