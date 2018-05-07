1be20977b5263673b9a4cb50c061a277b934cfc8
Sam Pierson <sam@sampierson.com>
2018-05-07 15:37:19 -0700

    Setup secrets with Terraform

---
+++ terraform/modules/upload-service/secrets.tf
@@ -0,0 +1,12 @@
+resource "aws_secretsmanager_secret" "secrets" {
+  name = "dcp/upload/${var.deployment_stage}/secrets"
+}
+
+resource "aws_secretsmanager_secret_version" "secrets" {
+  secret_id = "${aws_secretsmanager_secret.secrets.id}"
+  secret_string = <<SECRETS_JSON
+{
+  "bucket_name": "${aws_s3_bucket.upload_areas_bucket.bucket}",
+}
+SECRETS_JSON
+}
