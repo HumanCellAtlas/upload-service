f68704a1d9a0d7d13c9a197402fbe09f28721f97
Sam Pierson <sam@sampierson.com>
2018-05-07 17:27:15 -0700

    Have UploadConfig tests use deployment "bogo-env"
    as the test deployment secrets are about to be a real thing that
    contains the database uri.

--- tests/common/test_upload_config.py
+++ tests/common/test_upload_config.py
@@ -1,6 +1,3 @@
-from unittest.mock import patch
-import os
-
 from .. import UploadTestCaseUsingLiveAWS, EnvironmentSetup, fixture_file_path
 
 from upload.common.aws_secret import AwsSecret
@@ -11,9 +8,9 @@ class TestConfig(UploadTestCaseUsingLiveAWS):
 
     def setUp(self):
         super().setUp()
-        self.aws_secret1 = AwsSecret(name="dcp/upload/test/secrets")
+        self.aws_secret1 = AwsSecret(name="dcp/upload/bogo-env/secrets")
         self.aws_secret1.update('{"secret1":"cloud_value1"}')
-        self.aws_secret2 = AwsSecret(name="dcp/upload/test/secrets2")
+        self.aws_secret2 = AwsSecret(name="dcp/upload/bogo-env/secrets2")
         self.aws_secret2.update('{"secret2":"cloud_value2"}')
 
     def tearDown(self):
@@ -25,7 +22,7 @@ class TestConfig(UploadTestCaseUsingLiveAWS):
             'SECRET1': 'value_from_env',
             'CONFIG_SOURCE': None
         }):
-            config = UploadConfig(deployment='test')
+            config = UploadConfig(deployment='bogo-env')
             self.assertEqual(config.secret1, 'value_from_env')
 
     def test_from_file(self):
@@ -33,7 +30,7 @@ class TestConfig(UploadTestCaseUsingLiveAWS):
             'SECRET1': None,
             'CONFIG_SOURCE': fixture_file_path('config.js')
         }):
-            config = UploadConfig(deployment='test')
+            config = UploadConfig(deployment='bogo-env')
             self.assertEqual(config.secret1, 'value_from_file')
 
     def test_from_aws(self):
@@ -41,7 +38,7 @@ class TestConfig(UploadTestCaseUsingLiveAWS):
             'SECRET1': None,
             'CONFIG_SOURCE': None
         }):
-            config = UploadConfig(deployment='test', source='aws')
+            config = UploadConfig(deployment='bogo-env', source='aws')
             self.assertEqual(config.secret1, 'cloud_value1')
 
     def test_it_keeps_multiple_secrets_separate(self):
@@ -49,10 +46,10 @@ class TestConfig(UploadTestCaseUsingLiveAWS):
             'SECRET1': None,
             'CONFIG_SOURCE': None
         }):
-            config1 = UploadConfig(deployment='test')
+            config1 = UploadConfig(deployment='bogo-env')
             self.assertEqual(config1.secret1, 'cloud_value1')
 
-            config2 = UploadConfig(deployment='test', secret='secrets2')
+            config2 = UploadConfig(deployment='bogo-env', secret='secrets2')
             self.assertEqual(config2.secret2, 'cloud_value2')
 
             self.assertEqual(config1.secret1, 'cloud_value1')
