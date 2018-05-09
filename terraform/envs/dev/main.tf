958d8a704f0e003448b8a9dab1e5441ad348575e
Sam Pierson <sam@sampierson.com>
2018-05-09 12:29:18 -0700

    Terraform: make RDS instance count configurable
    and change it to 1 in dev/int/staging

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -35,4 +35,5 @@ module "upload-service" {
   vpc_rds_security_group_id = "${var.vpc_rds_security_group_id}"
   db_username = "${var.db_username}"
   db_password = "${var.db_password}"
+  db_instance_count = "${var.db_instance_count}"
 }
