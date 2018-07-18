433933de98367e2c6f90cc01125e5bb22cf91dbf
Sam Pierson <sam@sampierson.com>
2018-07-18 07:36:32 -0700

    deploy.sh: set -euo pipefail
    to attempt to get build to fail when deploy fails.
    Also, comment out build tagging, which fails at present.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 if [[ $# != 1 ]]; then
     echo "Usage: $(basename $0) stage"
     exit 1
@@ -17,20 +19,20 @@ function load_secrets(){
     source ${secrets_file}
 }
 
-function tag_deploy(){
-    TAG=`date -u +"${DEPLOYMENT_STAGE}-%Y%m%dT%H%M%SZ"`
-    echo "Tagging deploy ${TAG}"
-    curl -X POST \
-         --header "Authorization: token ${GITHUB_ACCESS_TOKEN}" \
-         --header "Content-Type: application/json" \
-         --data @- \
-         https://api.github.com/repos/HumanCellAtlas/upload-service/git/refs <<-EOF
-{
-  "ref": "refs/tags/${TAG}",
-  "sha": "`git rev-parse HEAD`"
-}
-EOF
-}
+#function tag_deploy(){
+#    TAG=`date -u +"${DEPLOYMENT_STAGE}-%Y%m%dT%H%M%SZ"`
+#    echo "Tagging deploy ${TAG}"
+#    curl -X POST \
+#         --header "Authorization: token ${GITHUB_ACCESS_TOKEN}" \
+#         --header "Content-Type: application/json" \
+#         --data @- \
+#         https://api.github.com/repos/HumanCellAtlas/upload-service/git/refs <<-EOF
+#{
+#  "ref": "refs/tags/${TAG}",
+#  "sha": "`git rev-parse HEAD`"
+#}
+#EOF
+#}
 
 function install_terraform() {
     echo "Installing Terraform..."
@@ -49,6 +51,7 @@ load_secrets
 install_terraform
 alembic -x db=${DEPLOYMENT_STAGE} -c=./config/database.ini upgrade head
 make deploy
-if [ ${DEPLOYMENT_STAGE} != dev ] ; then
-    tag_deploy
-fi
+
+#if [ ${DEPLOYMENT_STAGE} != dev ] ; then
+#    tag_deploy
+#fi
