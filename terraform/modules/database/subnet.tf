f90ada145c0961d2f16e9267aa50a93191030b77
Sam Pierson <sam@sampierson.com>
2018-08-30 08:37:35 -0700

    terraform: uniquely name rds db subnet group per env

--- terraform/modules/database/subnet.tf
+++ terraform/modules/database/subnet.tf
@@ -1,8 +1,8 @@
 resource "aws_db_subnet_group" "db_subnet_group" {
-  name       = "upload_db_subnet_group"
+  name       = "upload_db_subnet_group_${var.deployment_stage}"
   subnet_ids = ["${var.lb_subnet_ids}"]
 
   tags {
-    Name = "DCP Upload DB Subnet Group"
+    Name = "DCP Upload ${var.deployment_stage} DB Subnet Group"
   }
 }
