325968ee9398a7b2a3c286a948fd6752226c88c4
parthshahva <parthshahva@gmail.com>
2018-09-24 23:32:22 -0700

    patch for bucket name length limit

--- terraform/modules/upload-service/checksumming_lambda.tf
+++ terraform/modules/upload-service/checksumming_lambda.tf
@@ -105,7 +105,7 @@ output "upload_csum_lambda_role_arn" {
 
 
 resource "aws_s3_bucket" "lambda_area_bucket" {
-  bucket = "${var.bucket_name_prefix}checksum-lambda-deployment-${var.deployment_stage}"
+  bucket = "${var.bucket_name_prefix}csum-lambda-deployment-${var.deployment_stage}"
   acl = "private"
   force_destroy = "false"
   acceleration_status = "Enabled"
