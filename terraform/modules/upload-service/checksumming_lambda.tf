924f85776c5bfaa147487a2563b2f3c1822c0a83
parthshahva <parthshahva@gmail.com>
2018-11-08 09:39:44 -0800

    batch resource path now needs to be asterisk

--- terraform/modules/upload-service/checksumming_lambda.tf
+++ terraform/modules/upload-service/checksumming_lambda.tf
@@ -69,7 +69,7 @@ resource "aws_iam_role_policy" "upload_csum_lambda" {
         "batch:SubmitJob"
       ],
       "Resource": [
-        "arn:aws:batch:*:${local.account_id}:*"
+        "*"
       ]
     },
     {
