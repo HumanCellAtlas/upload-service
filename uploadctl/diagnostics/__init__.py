59a72a4f74f6f9340f15d2a0b86a1aef04ddd155
Sam Pierson <sam@sampierson.com>
2018-12-12 08:47:04 -0800

    Delete 'uploadctl diag job' functionality
    this has been replaced by 'dcpdig @upload batch_job=<uuid> --show log'

--- uploadctl/diagnostics/__init__.py
+++ uploadctl/diagnostics/__init__.py
@@ -1,5 +1,3 @@
-
-from .batch_job import BatchJobDumper
 from .db_dumper import DbDumper
 
 
@@ -11,10 +9,6 @@ class DiagnosticsCLI:
         diag_parser.set_defaults(command='diag')
         diag_subparsers = diag_parser.add_subparsers()
 
-        job_parser = diag_subparsers.add_parser('job', description="Get information about Batch jobs")
-        job_parser.set_defaults(command='diag', diag_command='job')
-        job_parser.add_argument('job_id', nargs='?', help="Show data bout this Batch job")
-
         job_parser = diag_subparsers.add_parser('db', description="Dump database records")
         job_parser.set_defaults(command='diag', diag_command='db')
         job_parser.add_argument('upload_area_id', nargs='?', help="Show record for this upload area")
@@ -22,9 +16,7 @@ class DiagnosticsCLI:
 
     @classmethod
     def run(cls, args):
-        if args.diag_command == 'job':
-            BatchJobDumper().describe_job(args.job_id)
-        elif args.diag_command == 'db':
+        if args.diag_command == 'db':
             if args.upload_area_id:
                 DbDumper().dump_one_area(args.upload_area_id, args.filename)
             else:
