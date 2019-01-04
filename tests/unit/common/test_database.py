718e56d77c43025ae8a4e6f368ca2ec59c8e1afb
Sam Pierson <sam@sampierson.com>
2019-01-04 15:58:11 -0700

    Move bogo config setup up into UploadTestCase
    so that UploadTestCaseUsingLiveAWS can benefit from it too.  Turns out
    there is nothing in the tests that use that superclass that need a legit
    config.

--- tests/unit/common/test_database.py
+++ tests/unit/common/test_database.py
@@ -11,15 +11,6 @@ class TestDatabase(UploadTestCaseUsingLiveAWS):
 
     def setUp(self):
         super().setUp()
-        # Config
-        self.config = UploadConfig()
-        self.config.set({
-            'bucket_name': "test_bucket_name",
-            'csum_upload_q_url': "bogo_url",
-            'area_deletion_q_url': "delete_sqs_url",
-            'area_deletion_lambda_name': "delete_lambda_name"
-        })
-
         self.area_id = str(uuid.uuid4())
         self.upload_area = UploadArea(self.area_id)
         self.db = UploadDB()
@@ -27,14 +18,14 @@ class TestDatabase(UploadTestCaseUsingLiveAWS):
         self.db.create_pg_record("upload_area", {
             "id": self.area_id,
             "status": "UNLOCKED",
-            "bucket_name": self.config.bucket_name
+            "bucket_name": self.upload_config.bucket_name
         })
 
     def test_get_pg_record(self):
         result = self.db.get_pg_record("upload_area", self.area_id)
 
         self.assertEqual(result["id"], self.area_id)
-        self.assertEqual(result["bucket_name"], self.config.bucket_name)
+        self.assertEqual(result["bucket_name"], self.upload_config.bucket_name)
         self.assertEqual(result["status"], "UNLOCKED")
 
     def test_update_pg_record(self):
@@ -44,10 +35,10 @@ class TestDatabase(UploadTestCaseUsingLiveAWS):
         self.db.update_pg_record("upload_area", {
             "id": self.area_id,
             "status": "LOCKED",
-            "bucket_name": self.config.bucket_name
+            "bucket_name": self.upload_config.bucket_name
         })
 
         after = self.db.get_pg_record("upload_area", self.area_id)
         self.assertEqual(after["id"], self.area_id)
-        self.assertEqual(after["bucket_name"], self.config.bucket_name)
+        self.assertEqual(after["bucket_name"], self.upload_config.bucket_name)
         self.assertEqual(after["status"], "LOCKED")
