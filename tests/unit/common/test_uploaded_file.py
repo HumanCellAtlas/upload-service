e7b35c37c361c3d869188c26355eb422d55dd627
Sam Pierson <sam@sampierson.com>
2019-01-16 10:48:52 -0700

    Refactor UploadedFile initialization signatures
    It now has:
    UploadedFile(upload_area, s3object)
    UploadedFile.create(upload_area, name, content_type, data)
    UploadedFile.from_s3_key(upload_area, s3_key)
    UploadedFile.from_db_id(db_id)
    
    The reason for this is that in some places now all we have is a db id,
    and we need to be able to retreive the file.

--- tests/unit/common/test_uploaded_file.py
+++ tests/unit/common/test_uploaded_file.py
@@ -1,11 +1,13 @@
+import os
 import uuid
 
-import boto3
 from sqlalchemy.orm.exc import NoResultFound
 
 from .. import UploadTestCaseUsingMockAWS
+from ... import FIXTURE_DATA_CHECKSUMS
 
 from upload.common.database_orm import DBSessionMaker, DbFile
+from upload.common.dss_checksums import DssChecksums
 from upload.common.upload_area import UploadArea
 from upload.common.uploaded_file import UploadedFile
 
@@ -15,63 +17,118 @@ class TestUploadedFile(UploadTestCaseUsingMockAWS):
     def setUp(self):
         super().setUp()
         self.db_session_maker = DBSessionMaker()
+        self.db = self.db_session_maker.session()
+
         self.upload_area_id = str(uuid.uuid4())
         self.upload_area = UploadArea(self.upload_area_id)
         self.upload_area.update_or_create()
+
+    def create_file_record(self, s3object):
+        record = DbFile(s3_key=s3object.key,
+                        s3_etag=s3object.e_tag.strip('\"'),
+                        name=os.path.basename(s3object.key),
+                        upload_area_id=self.upload_area.db_id,
+                        size=s3object.content_length)
+        self.db.add(record)
+        self.db.commit()
+        return record
 
     def tearDown(self):
         super().tearDown()
         pass
 
-    def test_from_s3_key__loads_data_and_creates_db_record_if_none_exists(self):
-        db = self.db_session_maker.session()
+    def test_create__creates_a_new_s3_object_and_db_record(self):
         filename = "file1"
-        file_content = "file1_body"
-        s3object = self.create_s3_object(object_key=f"{self.upload_area.uuid}/{filename}",
-                                         content=file_content)
-        s3_key = s3object.key
-        s3_etag = s3object.e_tag.strip('\"')
+        content_type = "application/octet-stream; dcp-type=data"
+        file_content = "file1_content"
 
-        with self.assertRaises(NoResultFound):
-            db.query(DbFile).filter(DbFile.s3_key == s3_key, DbFile.s3_etag == s3object.e_tag).one()
+        uf = UploadedFile.create(upload_area=self.upload_area,
+                                 name=filename,
+                                 content_type=content_type,
+                                 data=file_content)
 
-        uf = UploadedFile.from_s3_key(upload_area=self.upload_area, s3_key=s3_key)
-
-        self.assertEqual(filename, uf.name)
-        self.assertEqual(self.upload_area, uf.upload_area)
-        self.assertEqual(s3object, uf._s3obj)
-
-        record = db.query(DbFile).filter(DbFile.s3_key == s3_key, DbFile.s3_etag == s3_etag).one()
+        self.assertIsInstance(uf, UploadedFile)
+        # S3 Object
+        s3_key = f"{self.upload_area_id}/{filename}"
+        s3object = self.upload_bucket.Object(s3_key)
+        self.assertEqual(content_type, s3object.content_type)
+        self.assertEqual(file_content.encode('utf8'), s3object.get()['Body'].read())
+        # DB Record
+        record = self.db.query(DbFile).filter(DbFile.s3_key == s3_key,
+                                              DbFile.s3_etag == s3object.e_tag.strip('\"')).one()
         self.assertEqual(s3_key, record.s3_key)
         self.assertEqual(filename, record.name)
         self.assertEqual(s3object.e_tag.strip('\"'), record.s3_etag)
         self.assertEqual(len(file_content), record.size)
         self.assertEqual(self.upload_area.db_id, record.upload_area_id)
 
-    def test_from_s3_key__loads_data_but_doesnt_create_db_record_if_one_already_exists(self):
-        db = self.db_session_maker.session()
-        filename = "file2"
-        file_content = "file2_body"
-        s3object = self.create_s3_object(object_key=f"{self.upload_area.uuid}/{filename}",
-                                         content=file_content)
-        record = DbFile(s3_key=s3object.key, s3_etag=s3object.e_tag.strip('\"'),
-                        name=filename, upload_area_id=self.upload_area.db_id,
-                        size=len(file_content))
-        db.add(record)
-        db.commit()
-        record_count_before = db.query(DbFile).count()
+    def test_init__given_existing_entities__initializes_properties_correctly(self):
+        s3object = self.create_s3_object(f"{self.upload_area_id}/foo")
+        file_record = self.create_file_record(s3object)
 
-        UploadedFile.from_s3_key(s3_key=s3object.key, upload_area=self.upload_area)
+        uf = UploadedFile(self.upload_area, s3object=s3object)
 
-        self.assertEqual(record_count_before, db.query(DbFile).count())
+        # Links to objects
+        self.assertEqual(s3object, uf._s3obj)
+        self.assertEqual(self.upload_area, uf.upload_area)
+        self.assertIsInstance(uf.checksums, DssChecksums)
+        self.assertEqual(s3object, uf.checksums._s3obj)
+        # Persisted properties
+        self.assertEqual(file_record.id, uf.db_id)
+        self.assertEqual(s3object.key, uf.s3_key)
+        self.assertEqual(s3object.e_tag.strip('\"'), uf.s3_etag)
+        self.assertEqual(self.upload_area.db_id, uf._properties['upload_area_id'])
+        self.assertEqual(file_record.name, uf.name)
+        self.assertEqual(s3object.content_length, uf.size)
 
-    def test_with_data_in_paramaters_it_creates_a_new_file(self):
+    def test_init__when_no_db_record_exists__creates_a_db_record(self):
+        s3object = self.create_s3_object(f"{self.upload_area_id}/foo")
 
-        UploadedFile(upload_area=self.upload_area, name="file2",
-                     content_type="application/octet-stream; dcp-type=data", data="file2_content")
+        with self.assertRaises(NoResultFound):
+            self.db.query(DbFile).filter(DbFile.s3_key == s3object.key,
+                                         DbFile.s3_etag == s3object.e_tag.strip('\"')).one()
 
-        self.assertEqual("file2_content".encode('utf8'),
-                         self.upload_bucket.Object(f"{self.upload_area_id}/file2").get()['Body'].read())
+        uf = UploadedFile(upload_area=self.upload_area, s3object=s3object)
+
+        record = self.db.query(DbFile).filter(DbFile.s3_key == s3object.key,
+                                              DbFile.s3_etag == s3object.e_tag.strip('\"')).one()
+        self.assertEqual(record.id, uf.db_id)
+        self.assertEqual(s3object.key, record.s3_key)
+        self.assertEqual("foo", record.name)
+        self.assertEqual(s3object.e_tag.strip('\"'), record.s3_etag)
+        self.assertEqual(s3object.content_length, record.size)
+        self.assertEqual(self.upload_area.db_id, record.upload_area_id)
+
+    def test_init__doesnt_create_db_record_if_one_already_exists(self):
+        s3object = self.create_s3_object(f"{self.upload_area_id}/foo")
+        self.create_file_record(s3object)
+
+        record_count_before = self.db.query(DbFile).count()
+
+        UploadedFile(upload_area=self.upload_area, s3object=s3object)
+
+        self.assertEqual(record_count_before, self.db.query(DbFile).count())
+
+    def test_from_s3_key__initializes_correctly(self):
+        s3object = self.create_s3_object(f"{self.upload_area_id}/foo")
+        file_record = self.create_file_record(s3object)
+
+        uf = UploadedFile.from_s3_key(self.upload_area, s3_key=s3object.key)
+
+        self.assertEqual(self.upload_area, uf.upload_area)
+        self.assertEqual(s3object, uf._s3obj)
+        self.assertEqual(file_record.id, uf.db_id)
+
+    def test_from_db_id__initializes_correctly_and_figures_out_which_upload_area_to_use(self):
+        s3object = self.create_s3_object(f"{self.upload_area_id}/foo")
+        file_record = self.create_file_record(s3object)
+
+        uf = UploadedFile.from_db_id(file_record.id)
+
+        self.assertEqual(self.upload_area.uuid, uf.upload_area.uuid)
+        self.assertEqual(self.upload_area.db_id, uf.upload_area.db_id)
+        self.assertEqual(s3object, uf._s3obj)
+        self.assertEqual(file_record.id, uf.db_id)
 
     def test_refresh__picks_up_changed_content_type(self):
         filename = "file3"
@@ -93,9 +150,18 @@ class TestUploadedFile(UploadTestCaseUsingMockAWS):
         self.assertEqual(new_content_type, uf.content_type)
 
     def test_info(self):
-        # TODO
-        pass
+        file_content = "exquisite corpse"
+        s3object = self.create_s3_object(f"{self.upload_area_id}/foo", content=file_content)
+        file_record = self.create_file_record(s3object)
+        uf = UploadedFile(self.upload_area, s3object=s3object)
+        uf.checksums.compute()
 
-    def test_s3url(self):
-        # TODO
-        pass
+        self.assertEqual({
+            'upload_area_id': self.upload_area.uuid,
+            'name': file_record.name,
+            'size': s3object.content_length,
+            'content_type': s3object.content_type,
+            'url': f"s3://{s3object.bucket_name}/{s3object.key}",
+            'checksums': FIXTURE_DATA_CHECKSUMS[file_content]['checksums'],
+            'last_modified': s3object.last_modified.isoformat()
+        }, uf.info())
