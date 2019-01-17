800e3e020d2d03866df03dffde88a22695ddef3c
Sam Pierson <sam@sampierson.com>
2019-01-16 17:50:20 -0700

    tests: reuse FlaskApp
    recreating it in each test is slow, and reusing it doesn't
    appear to affect the results

--- tests/unit/lambdas/api_server/__init__.py
+++ tests/unit/lambdas/api_server/__init__.py
@@ -4,15 +4,24 @@ import yaml
 import connexion
 
 
+api_server_client = None
+
+
 def client_for_test_api_server():
-    flask_app = connexion.FlaskApp(__name__)
+    global api_server_client
 
-    swagger_yaml_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../../config/upload-api.yml'))
-    with open(swagger_yaml_path, mode='rb') as swagger_yaml:
-        contents = swagger_yaml.read()
-        swagger_string = contents.decode()
-        specification = yaml.safe_load(swagger_string)  # type: dict
-    specification['host'] = 'localhost'
-    flask_app.add_api(specification)
+    if not api_server_client:
+        flask_app = connexion.FlaskApp(__name__)
 
-    return flask_app.app.test_client()
+        swagger_yaml_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                         '../../../../config/upload-api.yml'))
+        with open(swagger_yaml_path, mode='rb') as swagger_yaml:
+            contents = swagger_yaml.read()
+            swagger_string = contents.decode()
+            specification = yaml.safe_load(swagger_string)  # type: dict
+        specification['host'] = 'localhost'
+        flask_app.add_api(specification)
+
+        api_server_client = flask_app.app.test_client()
+
+    return api_server_client
