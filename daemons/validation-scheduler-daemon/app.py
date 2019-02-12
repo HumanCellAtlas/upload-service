fb60985c4d197e1e54018e228b153e0425b65bcb
maniarathi <arathi.mani@chanzuckerberg.com>
2019-02-12 15:36:51 -0800

    Changing logging such that only the root logger has a handler and children loggers do not so that only the root logger will emit the payload.

--- daemons/validation-scheduler-daemon/app.py
+++ daemons/validation-scheduler-daemon/app.py
@@ -1,11 +1,12 @@
 import json
+import logging
 
-from upload.common.uploaded_file import UploadedFile
-from upload.common.validation_scheduler import ValidationScheduler
+from upload.common.logging import configure_logger
 from upload.common.upload_area import UploadArea
-from upload.common.logging import get_logger
+from upload.common.validation_scheduler import ValidationScheduler
 
-logger = get_logger(__name__)
+configure_logger()
+logger = logging.getLogger(__name__)
 
 
 # This lambda function is invoked by messages in the the validation_queue (AWS SQS).
