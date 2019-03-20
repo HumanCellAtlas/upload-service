e3021937676430d16c9d195d27ce39e268c28a65
Parth Shah <pshah@chanzuckerberg.com>
2019-03-20 12:12:30 -0700

    update health check to not be nested within /v1 and check health of db (#331)
    * update health check to not be nested within v1 and check health of db
    
    * better docstring for health check function

--- tests/unit/lambdas/api_server/test_health.py
+++ tests/unit/lambdas/api_server/test_health.py
@@ -7,6 +7,6 @@ class TestHealthcheckEndpoint(UploadTestCaseUsingMockAWS):
     def test_heathcheck_endpoint(self):
         self.client = client_for_test_api_server()
 
-        response = self.client.get(f"/v1/health")
+        response = self.client.get(f"/health")
 
         self.assertEqual(response.status_code, 200)
