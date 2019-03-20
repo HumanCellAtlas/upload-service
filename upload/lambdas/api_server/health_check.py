ea739f6409ef25b55096a04b6e771905fd0ebac4
parthshahva <parthshahva@gmail.com>
2019-03-20 11:17:31 -0700

    better docstring for health check function

--- upload/lambdas/api_server/health_check.py
+++ upload/lambdas/api_server/health_check.py
@@ -6,7 +6,11 @@ from upload.lambdas.api_server import return_exceptions_as_http_errors
 
 @return_exceptions_as_http_errors
 def health():
-    # This query checks the health of underlying db infrastructure (pgbouncer + rds)
+    """
+    This api endpoint is invoked by the dcp wide status monitoring system.
+    This function checks the health of underlying api gateway and db infrastructure.
+    Running a simple query confirms that ecs pgbouncer is up running and talking to rds.
+    """
     db_health_check_query = "SELECT count(*) from upload_area;"
     UploadDB().run_query(db_health_check_query)
     return requests.codes.ok
