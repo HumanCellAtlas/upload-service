ff3e6efe00526a17771d092e1dd76d2881165cd7
Sam Pierson <sam@sampierson.com>
2019-03-21 17:21:33 -0700

    Alphabetize secrets and update BOGO_CONFIG
    Please keep these to lists in sync so we can test.

--- terraform/modules/upload-service/secrets.tf
+++ terraform/modules/upload-service/secrets.tf
@@ -4,21 +4,26 @@ resource "aws_secretsmanager_secret" "secrets" {
 
 resource "aws_secretsmanager_secret_version" "secrets" {
   secret_id = "${aws_secretsmanager_secret.secrets.id}"
+  //
+  // HEY YOU!  DEVELOPER!
+  //
+  // Keep this list in sync with BOGO_CONFIG in tests/unit/__init__.py
+  //
   secret_string = <<SECRETS_JSON
 {
-  "bucket_name": "${aws_s3_bucket.upload_areas_bucket.bucket}",
-  "validation_job_q_arn": "${aws_batch_job_queue.validation_job_q.arn}",
-  "validation_job_role_arn": "${aws_iam_role.validation_job_role.arn}",
-  "csum_job_q_arn": "${aws_batch_job_queue.csum_job_q.arn}",
-  "csum_upload_q_url": "${aws_sqs_queue.upload_queue.id}",
+  "api_key": "${var.ingest_api_key}",
   "area_deletion_q_url": "${aws_sqs_queue.area_deletion_queue.id}",
   "area_deletion_lambda_name": "${aws_lambda_function.area_deletion_lambda.function_name}",
+  "bucket_name": "${aws_s3_bucket.upload_areas_bucket.bucket}",
+  "csum_job_q_arn": "${aws_batch_job_queue.csum_job_q.arn}",
   "csum_job_role_arn": "${aws_iam_role.csum_job_role.arn}",
-  "upload_submitter_role_arn": "${aws_iam_role.upload_submitter.arn}",
-  "api_key": "${var.ingest_api_key}",
+  "csum_upload_q_url": "${aws_sqs_queue.upload_queue.id}",
+  "ingest_api_host": "${var.ingest_api_host}",
   "slack_webhook": "${var.slack_webhook}",
   "staging_bucket_arn": "${var.staging_bucket_arn}",
-  "ingest_api_host": "${var.ingest_api_host}",
+  "upload_submitter_role_arn": "${aws_iam_role.upload_submitter.arn}",
+  "validation_job_q_arn": "${aws_batch_job_queue.validation_job_q.arn}",
+  "validation_job_role_arn": "${aws_iam_role.validation_job_role.arn}",
   "validation_q_url": "${aws_sqs_queue.validation_queue.id}"
 }
 SECRETS_JSON
