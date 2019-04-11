81be75cb6d2851cd8df7123063f32e9fe32efdd7
Matt Weiden <538456+mweiden@users.noreply.github.com>
2019-04-11 16:59:25 -0700

    Jump through some hoops

--- terraform/modules/upload-service/dns.tf
+++ terraform/modules/upload-service/dns.tf
@@ -18,6 +18,15 @@ resource "aws_api_gateway_domain_name" "upload" {
   }
 }
 
+data "external" "api_gateway" {
+  program = ["${path.cwd}/../../../scripts/get_api_id", "--json"]
+
+  query = {
+    api_gateway_name = "upload.lambdas.api_server"
+    lambda_name = "${aws_lambda_function.upload_api_lambda.function_name}"
+  }
+}
+
 resource "aws_route53_record" "upload" {
   name    = "${aws_api_gateway_domain_name.upload.domain_name}"
   type    = "A"
@@ -28,4 +37,10 @@ resource "aws_route53_record" "upload" {
     name                   = "${aws_api_gateway_domain_name.upload.cloudfront_domain_name}"
     zone_id                = "${aws_api_gateway_domain_name.upload.cloudfront_zone_id}"
   }
+}
+
+resource "aws_api_gateway_base_path_mapping" "status_api" {
+  api_id      = "${lookup(data.external.api_gateway.result, "api_id")}"
+  stage_name  = "${var.deployment_stage}"
+  domain_name = "${aws_api_gateway_domain_name.upload.domain_name}"
 }
